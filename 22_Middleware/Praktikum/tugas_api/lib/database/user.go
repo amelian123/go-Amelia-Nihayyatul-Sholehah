8f8163c41a498cfc20b2ba344f0646ca8c2abc7d
amelian123 <102641033+amelian123@users.noreply.github.com>
2023-04-17 23:27:30 +0700

    add praktikum

---
+++ 22_Middleware/Praktikum/tugas_api/lib/database/user.go
@@ -0,0 +1,15 @@
+package database
+
+import (
+	"api/config"
+	"api/models"
+)
+
+func GetUsers() (interface{}, error) {
+	var users []models.User
+
+	if e := config.DB.Find(&users).Error; e != nil {
+		return nil, e
+	}
+	return users, nil
+}
