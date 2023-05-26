83b87d1929f39920a0aef5e37dd475b639db6251
amelian123 <102641033+amelian123@users.noreply.github.com>
2023-05-26 22:31:04 +0700

    Delete code_competence directory

--- code_competence/constants/constans.go
+++
@@ -1,4 +0,0 @@
-package constants
-
-// Secret Key Jwt
-const SECRET_JWT = "secretjwt"
--- code_competence/models/categoryItems.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type CategoryItems struct {
-	ID           uint   `json:"id" form:"id" gorm:"primarykey"`
-	NameCategory string `json:"name" form:"name"`
-	Items        []Item `gorm:"foreignKey:CategoryID"`
-}
